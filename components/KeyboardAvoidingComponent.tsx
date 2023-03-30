868d3ec60410251810259a3e40dd00cbc8a8887f
David Bishop <david.bishop@riskaware.co.uk>
2023-03-29 19:21:30 -0600

    android fixes

--- components/KeyboardAvoidingComponent.tsx
+++ components/KeyboardAvoidingComponent.tsx
@@ -1,61 +1,76 @@
-import { TouchableWithoutFeedback, Keyboard, StyleSheet, KeyboardAvoidingView, Platform, View, SafeAreaView, Animated, Text } from 'react-native';
-import React, { useEffect, useState } from 'react'
-import { useHeaderHeight } from '@react-navigation/elements';
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  SafeAreaView,
+  Animated,
+  ScrollView,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {useHeaderHeight} from '@react-navigation/elements';
 
 interface Props {
-  children: JSX.Element | JSX.Element[]
+  children: JSX.Element | JSX.Element[];
 }
 
-const KeyboardAvoidingComponent = ({ children }: Props) => {
+const KeyboardAvoidingComponent = ({children}: Props) => {
+  // android
+  if (Platform.OS === 'android') {
+    return (
+      <View style={{flex: 1}}>
+        <View style={styles.inner}>{children}</View>
+      </View>
+    );
+  }
 
+  // iOS
   // get height of header
-  const headerHeight = useHeaderHeight()
-
-  const [height, heightSet] = useState(0)
+  const headerHeight = useHeaderHeight();
+  // store height of buffer that pushes content into view
+  const [height, heightSet] = useState(0);
   useEffect(() => {
-    Keyboard.addListener("keyboardWillShow", () => heightSet(headerHeight));
-    Keyboard.addListener("keyboardWillHide", () => heightSet(0));
+    Keyboard.addListener('keyboardWillShow', () => heightSet(headerHeight));
+    Keyboard.addListener('keyboardWillHide', () => heightSet(0));
 
     return () => {
-      Keyboard.removeAllListeners("keyboardWillShow")
-      Keyboard.removeAllListeners("keyboardWillHide")
-    }
-  }, [Keyboard])
+      Keyboard.removeAllListeners('keyboardWillShow');
+      Keyboard.removeAllListeners('keyboardWillHide');
+    };
+  }, [Keyboard]);
 
-  const [bufferHeight] = useState(new Animated.Value(0))
+  // store animated height value and animate when height changes
+  const [bufferHeight] = useState(new Animated.Value(0));
   useEffect(() => {
     Animated.timing(bufferHeight, {
       toValue: height,
       useNativeDriver: false,
-      duration: 500
-    }).start()
-  }, [height])
+      duration: 500,
+    }).start();
+  }, [height]);
 
   return (
-    <KeyboardAvoidingView enabled={true} style={{ flex: 1 }} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+    <KeyboardAvoidingView enabled={true} style={{flex: 1}} behavior="padding">
       <SafeAreaView style={styles.container}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View style={{flex: 1}}>
-            <Text>{JSON.stringify(height, null, 2)}</Text>
-          <View style={styles.inner}>
-            {children}
-          </View>
-            <Animated.View style={{ height: bufferHeight }} />
-          </View>
+          <ScrollView style={{flex: 1}}>
+            <View style={styles.inner}>{children}</View>
+            <Animated.View style={{height: bufferHeight}} />
+          </ScrollView>
         </TouchableWithoutFeedback>
       </SafeAreaView>
     </KeyboardAvoidingView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
+  container: {flex: 1},
   inner: {
     flex: 1,
     padding: 16,
-    backgroundColor: "red",
-    borderBottomWidth: 2
   },
-})
+});
 
-export default KeyboardAvoidingComponent
+export default KeyboardAvoidingComponent;
